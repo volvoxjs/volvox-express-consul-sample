48b9d64bb5ad22b4403444dd67c6feb24fcc71ec
Ziya SARIKAYA <sarikayaziya@gmail.com>
2016-08-02 20:08:27 +0200

    Update client.js

--- client.js
+++ client.js
@@ -1,25 +1,18 @@
 import "babel-polyfill";
 
-import {Volvox} from 'volvox-core';
+import Volvox from 'volvox-core';
 import vconsul from 'volvox-consul';
 
 import Logger from './logger'
 import request from 'request-promise';
 
 async function init() {
-    try {
-        let clusterProvider = new ConsulProvider(new ConsulRestClient(), new Logger(), new RandomProvider());
-
-        let cluster = new Cluster(clusterProvider);
-
-        await cluster.bootstrapClient();
-        let instance = await cluster.findServiceInstanceAsync("customers");
-
-        let body = await request(`http://${instance.address}:${instance.port}/customers`);
-        console.log(body);
-    } catch (error) {
-        console.error(error);
-    }
+    let volvox = new Volvox(vconsul());
+    await volvox.bootstrapClient();
+    
+    let instance = await volvox.findServiceInstanceAsync("customers");
+    let body = await request(`http://${instance.address}:${instance.port}/customers`);
+    console.log(body);
 }
 
 init();
